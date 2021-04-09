30bb96e2580de99c333735564854d3ee322af2c5
NyctibiusVII <matheus.dev.07@gmail.com>
2021-04-09 05:29:17 -0300

    'EmBreve' component created

---
+++ src/components/EmBreve.tsx
@@ -0,0 +1,18 @@
+export function EmBreve() {
+    return(
+        <div className='containerEmBreve'>
+            <span>Em breve...</span>
+            
+            <style jsx>{`
+                .containerEmBreve {
+                  width: 100%;
+                  height: 100%;
+
+                  display: flex;
+                  align-items: center;
+                  justify-content: center;
+                }
+            `}</style>
+        </div>
+    )
+}
