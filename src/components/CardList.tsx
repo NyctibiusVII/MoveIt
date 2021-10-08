0f2ebe08753fe5af24f837bb1187fa7dcfe5d33e
NyctibiusVII <matheus.dev.07@gmail.com>
2021-10-08 16:06:50 -0300

    CardUser (Desktop) refactoring*

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -8,17 +8,21 @@ import styles from '../styles/components/CardList.module.css'
 
 export function CardList() {
     return (
-        <div className={styles.cardListContainer}>
-            <header>
-                <p>VOCÊ</p>
-
-                <div>
+        <>
+            <div className={styles.cardListContainer}>
+                <div className={styles.you}>
+                    <p>VOCÊ</p>
+                </div>
+                <div className={styles.void} />
+                <div className={styles.challenges}>
                     <p>DESAFIOS</p>
+                </div>
+                <div className={styles.experience}>
                     <p>EXPERIÊNCIA</p>
                 </div>
-            </header>
+            </div>
 
             <CardUser />
-        </div>
+        </>
     )
 }
